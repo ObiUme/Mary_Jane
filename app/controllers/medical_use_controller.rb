7b03a5140e4b5d71f8c9e068fbe63c77e2c48662
Obi <mr.obiume@gmail.com>
2021-08-26 18:07:33 -0700

    new path

--- app/controllers/medical_use_controller.rb
+++ app/controllers/medical_use_controller.rb
@@ -1,7 +1,7 @@
 class MedicalUseController < ApplicationController
     get "/medical_use" do
         medical_uses = MedicalUse.all 
-        medical_uses.to_json
+        medical_uses.to_json(:include => :strains)
     end
     
 end
